3cf286c448055076097a07db74bbaadd5565dd0f
piowyg <piotrwygocki12@gmail.com>
2025-05-07 20:39:46 +0200

    Diabetix#12 implement get user information endpoint from JWT

--- src/main/kotlin/pl/diabetix/diabetix/api/web/UserController.kt
+++ src/main/kotlin/pl/diabetix/diabetix/api/web/UserController.kt
@@ -1,49 +1,27 @@
 package pl.diabetix.diabetix.api.web
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import jakarta.validation.Valid
-import jakarta.validation.constraints.Email
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
-import jakarta.validation.constraints.Past
-import org.springframework.http.HttpStatus
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.web.bind.annotation.*
-import pl.diabetix.diabetix.application.UserDataFactory
 import pl.diabetix.diabetix.application.UserService
 import pl.diabetix.diabetix.domain.User
+import pl.diabetix.diabetix.domain.UserNotFoundException
+import java.security.Principal
 import java.time.LocalDate
 
 @RestController
 @RequestMapping("/users")
 class UserController(
-    private val userService: UserService,
-    private val userDataFactory: UserDataFactory
+    private val userService: UserService
 ) {
-
-    // TODO:
-    //  check how return 201 status
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    fun createUser(@Valid @RequestBody request: CreateUserRequest) =
-        userDataFactory.convertToCreateUser(request)
-            .let { userService.create(it).toResponse() }
+    @GetMapping("/account")
+    fun getAccount(principal: Principal): UserResponse? {
+        if (principal is AbstractAuthenticationToken) {
+            return userService.getUserFromAuthentication(principal).toResponse()
+        } else {
+            throw UserNotFoundException("User could not be found")
+        }
+    }
 }
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class CreateUserRequest(
-    @Email
-    val email: String,
-    @NotBlank(message = "Password cannot be blank")
-    val password: String,
-    @NotNull
-    val login: String,
-    @NotNull
-    val name: String,
-    @NotNull
-    val surname: String,
-    @Past(message = "Birthdate must be in the past")
-    val birthdate: LocalDate
-)
 
 fun User.toResponse() = UserResponse(
     id = this.id,
