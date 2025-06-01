28fdbffd45dcdf05a0162e4819906c478ab7b939
piowyg <piotrwygocki12@gmail.com>
2025-06-01 23:28:50 +0200

    remove old classes

--- src/test/kotlin/pl/diabetix/diabetix/TestDiabetixApplication.kt
+++
@@ -1,9 +0,0 @@
-package pl.diabetix.diabetix
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-    fromApplication<DiabetixApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
