91605480bba24b8ff46809503dd7d23e2066c7ac
piowyg <piotrwygocki12@gmail.com>
2025-06-01 23:22:33 +0200

    create user endpoint ability

---
+++ src/integration/kotlin/pl/diabetix/diabetix/api/web/UserEndpointAbility.kt
@@ -0,0 +1,36 @@
+package pl.diabetix.diabetix.api.web
+
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+//import pl.diabetix.diabetix.AuthenticationUtils
+import pl.diabetix.diabetix.UrlUtils
+
+
+@Component
+class UserEndpointAbility(
+    private val restTemplate: RestTemplate, // TODO: change to WebClient or something
+    private val urlUtils: UrlUtils,
+//    private val authenticationUtils: AuthenticationUtils
+) {
+
+    fun callUserInfo(): ResponseEntity<UserResponse> {
+        val httpEntity = headers()
+        return restTemplate.exchange(
+            urlUtils.createUrl("/users/account"),
+            org.springframework.http.HttpMethod.GET,
+            HttpEntity(null, httpEntity),
+            UserResponse::class.java
+        )
+    }
+
+    private fun headers(): HttpHeaders {
+        val headers = HttpHeaders()
+//        headers.setBearerAuth(authenticationUtils.getToken())
+        headers.contentType = MediaType.APPLICATION_JSON
+        return headers
+    }
+}
