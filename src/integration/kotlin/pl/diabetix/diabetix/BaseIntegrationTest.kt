f788166d3b41d22481c8ac6ec035224a6a0ca63b
piowyg <piotrwygocki12@gmail.com>
2025-06-07 22:07:37 +0200

    diabetix#13| enhance BaseIntegrationTest with Keycloak integration and update MongoDB version

--- src/integration/kotlin/pl/diabetix/diabetix/BaseIntegrationTest.kt
+++ src/integration/kotlin/pl/diabetix/diabetix/BaseIntegrationTest.kt
@@ -1,24 +1,31 @@
 package pl.diabetix.diabetix
 
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.TestInfo
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MongoDBContainer
-import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
+import pl.diabetix.diabetix.KeycloakContainerInitializer.Companion.getKeycloakContainer
 import pl.diabetix.diabetix.config.TestApplicationConfig
 import pl.diabetix.diabetix.infrastructure.FixedTestDateTimeProvider
 import java.time.OffsetDateTime
 
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(
     classes = [DiabetixApplication::class, TestApplicationConfig::class],
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
+@ContextConfiguration(initializers = [KeycloakContainerInitializer::class])
 class BaseIntegrationTest {
 
     @Autowired
@@ -26,6 +33,13 @@ class BaseIntegrationTest {
 
     @Autowired
     private lateinit var dateTimeProvider: FixedTestDateTimeProvider
+
+    @Autowired
+    private  lateinit var keycloakTestUtils: KeycloakTestUtils
+
+    fun getAccessToken(): String {
+        return keycloakTestUtils.getAccessToken()
+    }
 
     fun givenTime(dateTime: OffsetDateTime) {
         dateTimeProvider.setDateTime(dateTime)
@@ -35,16 +49,7 @@ class BaseIntegrationTest {
     //  integrate keycloak
 
     companion object {
-        private const val MONGO_DB_DOCKER_IMAGE = "mongo:7.0.5"
-        private const val POSTGRES_DOCKER_IMAGE = "postgres:16.2"
-
-        @JvmStatic
-        val postgres: PostgreSQLContainer<*> = PostgreSQLContainer(POSTGRES_DOCKER_IMAGE)
-            .withDatabaseName("keycloak")
-            .withUsername("keycloak")
-            .withPassword("keycloak")
-            .withReuse(true)
-            .apply { start() }
+        private const val MONGO_DB_DOCKER_IMAGE = "mongo:8.0.4"
 
         @JvmStatic
         val mongo: MongoDBContainer = MongoDBContainer(
@@ -65,5 +70,10 @@ class BaseIntegrationTest {
         if (info.tags.contains("cleanRepo"))  {
             repositories.forEach { it.deleteAll() }
         }
+    }
+
+    @AfterAll
+fun tearDownKeycloak() {
+        getKeycloakContainer().stop()
     }
 }
