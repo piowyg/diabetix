a967fa6b03f39daa4a224e1a45dae1908b112113
piowyg <piotrwygocki12@gmail.com>
2025-09-07 20:07:59 +0200

    add builder

--- src/integration/kotlin/pl/diabetix/diabetix/BaseEndpointAbility.kt
+++ src/integration/kotlin/pl/diabetix/diabetix/BaseEndpointAbility.kt
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
+import org.springframework.test.web.reactive.server.WebTestClient
 
 @Component
-class BaseEndpointAbility{
+class BaseEndpointAbility(){
+
     @Autowired
-    lateinit var restTemplate: RestTemplate // TODO: change to WebClient or something
+    lateinit var webClient: WebTestClient
     @Autowired
     private lateinit var urlUtils: UrlUtils
     @Autowired
@@ -19,7 +20,7 @@ class BaseEndpointAbility{
         return urlUtils.createUrl(path)
     }
 
-    fun headers(userName: String = "user", password: String = "user"): HttpHeaders {
+    fun bearerAuth(userName: String = "user", password: String = "user"): HttpHeaders {
         val headers = HttpHeaders()
         headers.setBearerAuth(keycloakTestUtils.getAccessToken(userName, password))
         headers.contentType = MediaType.APPLICATION_JSON
---
+++ src/test/kotlin/pl/diabetix/diabetix/InfusionSetBuilder.kt
@@ -0,0 +1,32 @@
+package pl.diabetix.diabetix
+
+import pl.diabetix.diabetix.domain.InfusionSet
+import java.time.LocalDate
+
+class InfusionSetBuilder {
+    private var id: String = "123"
+    private var bodyLocation: String = "arm"
+    private var userId: String = "1234"
+    private var insertionDate: LocalDate = LocalDate.of(2025, 6, 8,)
+    private var removalDeadline: LocalDate = LocalDate.of(2025, 6, 11)
+    private var removalDate: LocalDate? = null
+    private var isActive: Boolean = true
+
+    fun id(id: String) = apply { this.id = id }
+    fun bodyLocation(bodyLocation: String) = apply { this.bodyLocation = bodyLocation }
+    fun userId(userId: String) = apply { this.userId = userId }
+    fun insertionDate(insertionDate: LocalDate) = apply { this.insertionDate = insertionDate }
+    fun removalDeadline(removalDeadline: LocalDate) = apply { this.removalDeadline = removalDeadline }
+    fun removalDate(removalDate: LocalDate?) = apply { this.removalDate = removalDate }
+
+    fun build(): InfusionSet = InfusionSet(
+        id = id,
+        bodyLocation = bodyLocation,
+        userId = userId,
+        insertionDate = insertionDate,
+        removalDeadline = removalDeadline,
+        removalDate = removalDate,
+        isActive = isActive,
+    )
+}
+
