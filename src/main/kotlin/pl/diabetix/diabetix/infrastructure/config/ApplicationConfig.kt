442a94b74a8e143f1f85f947c61a68f58f61b8d1
piowyg <piotrwygocki12@gmail.com>
2025-09-07 11:58:07 +0200

    define objectMapper

--- src/main/kotlin/pl/diabetix/diabetix/infrastructure/config/ApplicationConfig.kt
+++ src/main/kotlin/pl/diabetix/diabetix/infrastructure/config/ApplicationConfig.kt
@@ -1,8 +1,12 @@
 package pl.diabetix.diabetix.infrastructure.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import pl.diabetix.diabetix.domain.DefaultDateTimeProvider
 import pl.diabetix.diabetix.infrastructure.ApplicationProperties
 
@@ -11,5 +15,14 @@ import pl.diabetix.diabetix.infrastructure.ApplicationProperties
 class ApplicationConfig {
 
     @Bean
-    fun dateTimeProvider() = DefaultDateTimeProvider("UTC")
+    fun dateTimeProvider() = DefaultDateTimeProvider()
+
+    @Primary
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        val objectMapper = ObjectMapper()
+        objectMapper.registerModule(KotlinModule.Builder().build())
+        objectMapper.registerModule(JavaTimeModule())
+        return objectMapper
+    }
 }
